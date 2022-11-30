b7222101888942a46951c4cf1ea12bef77e8bc20
iamPres <prestowillis@gmail.com>
2022-11-29 21:59:36 -0600

    organism

--- display.py
+++ display.py
@@ -2,6 +2,8 @@ import pygame
 import numpy as np
 import uuid
 import collections
+import time
+from copy import deepcopy
 
 pygame.init()
 
@@ -15,11 +17,11 @@ blue = (0,0,255)
 gameDisplay = pygame.display.set_mode((800,800))
 gameDisplay.fill(black)
 
+ENERGY_LOSS = 0.4
 ENERGY_DECAY = 0.1
-CELL_LIMIT = 800
-ORIGIN = [400, 0]
-
 ENERGY_CONSERVED = 0.5
+CELL_LIMIT = 10000
+ORIGIN = [400, 100]
 
 pixAr = pygame.PixelArray(gameDisplay)
 
@@ -28,95 +30,126 @@ def render_cells(org):
         for idy in list(org.positions[idx]):
             cell = org.positions[idx][idy]
             if cell.x < len(pixAr) and cell.y < len(pixAr[0]) and cell.dead == False:
-                pixAr[cell.x][cell.y] = (0, int(cell.energy*255), 0)
+                color = int(cell.energy*255)
+                if color > 255: color = 255
+                pixAr[cell.x][cell.y] = (0, color, 0)
             elif cell.dead:
                 pixAr[cell.x][cell.y] = black
 
 def handle_pygame():
     energy_mod = 0
+    pos = None
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
             if event.mod == pygame.KMOD_NONE:
                 pass
             else:
                 if event.mod & pygame.KMOD_LSHIFT:
-                    energy_mod += 0.1
+                    energy_mod += 1
                 if event.mod & pygame.KMOD_RSHIFT:
-                    energy_mod -= 0.1
+                    energy_mod -= 1
                 if event.mod & pygame.KMOD_CTRL:
                     energy_mod -= 1
+        if event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-    return energy_mod
+    return (energy_mod, pos)
 
 class Organism:
     def __init__(self, origin) -> None:
         self.origin = origin
         self.originCell = Cell(origin[0], origin[1], uuid.uuid1())
+        self.originCell.ableToReplicate = [1,0,0,0]
         self.positions = collections.defaultdict(dict)
         self.members = 0
         self.positions[self.originCell.x][self.originCell.y] = self.originCell
-        self.energyConserved = ENERGY_CONSERVED
         self.previous_energy_matrix = []
 
-    def tick(self, ticks):
-        if ticks < 800:
-            food_energy = [1]*self.members
-        else:
-            food_energy = [0.04]*self.members
-            for i in range(10):
-                food_energy[(ticks+(100*i))%799] = 1
+    def tick(self, ticks, food_energy):
+        energies = self.compute_energies(deepcopy(food_energy), ticks)
+        updated_positions = deepcopy(self.positions)
+        self.compute_cells(updated_positions, energies)
 
-        energies = collections.defaultdict(dict)#self.compute_energies(food_energy, ticks)
-        updated_positions = self.positions
+    def compute_cells(self, cells, energies):
         for idx in list(self.positions):
             for idy in list(self.positions[idx]):
-                cell = self.positions[idx][idy]
-                energies[cell.x][cell.y] = 0
-                newCell = cell.tick(energies[cell.x][cell.y])
+                cell = cells[idx][idy]
+                newCells = cell.tick(energies[cell.x][cell.y])
                 if not cell.dead:
-                    if newCell is not None:
+                    if len(newCells) > 0:
                         if self.members < CELL_LIMIT:
-                            self.members += 1
-                            updated_positions[newCell.x][newCell.y] = newCell
-                            cell.ableToReplicate -= 1
+                            for newCell in newCells:
+                                self.members += 1
+                                cells[newCell.x][newCell.y] = newCell
                 if cell.dead: 
                     self.members -= 1
-                    updated_positions[cell.x].pop(cell.y)
-        self.positions = updated_positions
+                    cells[cell.x].pop(cell.y)
+        self.positions = deepcopy(cells)
 
     def compute_energies(self, energy_matrix, ticks):
-        ENERGY_UP = 0.45
-        ENERGY_DOWN = 0.45
+        available_energy = 1 - ENERGY_LOSS
+
+        ENERGY_X_Y = 0.24
+        ENERGY_X_NEG_Y = 0.24
+        ENERGY_NEG_X_Y = 0.24
+        ENERGY_NEG_X_NEG_Y = 0.24
+
         if ticks == 0:
-            self.previous_energy_matrix = [energy_matrix[0]]*3
+            self.previous_energy_matrix = deepcopy(energy_matrix)
             return self.previous_energy_matrix
-
         elif ticks > 0:
-            new_energy_matrix = [0]*self.members
-            if len(self.cells) > 3:
-                for cell_index in range(self.members):
-                    if cell_index < len(self.previous_energy_matrix)-1:
-                        energy_down = self.previous_energy_matrix[cell_index+1]
-                        energy_up = self.previous_energy_matrix[cell_index-1]
-                    else:
-                        energy_down = 0
-                        energy_up = 0
-                    if cell_index == 0:
-                        new_energy_matrix[cell_index] = energy_down * ENERGY_DOWN
-                    if cell_index == len(self.cells)-1:
-                        new_energy_matrix[cell_index] = energy_up * ENERGY_UP
-                    else:
-                        new_energy_matrix[cell_index] = (energy_up * ENERGY_UP) + (energy_down * ENERGY_DOWN)
-                total_energy = np.add(new_energy_matrix, energy_matrix)
-                for e in range(len(total_energy)):
-                    if total_energy[e] > 1: total_energy[e] = 1
-                    if total_energy[e] < 0: total_energy[e] = 0
+            new_energy_matrix = energy_matrix
+            if self.members > 0:
+                for idx in list(self.previous_energy_matrix):
+                    for idy in list(self.previous_energy_matrix[idx]):
+                        cell_energy = self.previous_energy_matrix[idx][idy]
+                        if idx in self.positions:
+                            if idy in self.positions[idx]:
+                                self.calculate_replication_possibilities(idx, idy, new_energy_matrix)
+                        
+                                peer_energies = [0,0,0,0]
+                                self_energy = self.positions[idx][idy].energy
 
-                self.previous_energy_matrix = total_energy
+                                if idx+1 in new_energy_matrix:
+                                    if idy+1 in new_energy_matrix[idx+1]:
+                                        peer_energies[0] = self.positions[idx+1][idy+1].energy
+                                    if idy-1 in new_energy_matrix[idx+1]:
+                                        peer_energies[1] = self.positions[idx+1][idy-1].energy
+
+                                if idx-1 in new_energy_matrix:
+                                    if idy+1 in new_energy_matrix[idx-1]:
+                                        peer_energies[2] = self.positions[idx-1][idy+1].energy
+                                    if idy-1 in new_energy_matrix[idx-1]:
+                                        peer_energies[3] = self.positions[idx-1][idy-1].energy
+
+
+                                if idx+1 in new_energy_matrix:
+                                    if idy+1 in new_energy_matrix[idx+1]:
+                                        new_energy_matrix[idx+1][idy+1] += cell_energy * ENERGY_X_Y
+                                    if idy-1 in new_energy_matrix[idx+1]:
+                                        new_energy_matrix[idx+1][idy-1] += cell_energy * ENERGY_X_NEG_Y
+
+                                if idx-1 in new_energy_matrix:
+                                    if idy+1 in new_energy_matrix[idx-1]:
+                                        new_energy_matrix[idx-1][idy+1] += cell_energy * ENERGY_NEG_X_Y  
+                                    if idy-1 in new_energy_matrix[idx-1]:
+                                        new_energy_matrix[idx-1][idy-1] += cell_energy * ENERGY_NEG_X_NEG_Y
+                self.previous_energy_matrix = deepcopy(new_energy_matrix)
             else: return energy_matrix
-            return self.previous_energy_matrix
+            return new_energy_matrix
+    
+    def calculate_replication_possibilities(self, x, y, energies):
+        self.positions[x][y].ableToReplicate = [0]*4
+        if not y+1 in energies[x+1]:
+            self.positions[x][y].ableToReplicate[0] = 1
+        if not y-1 in energies[x+1]:
+            self.positions[x][y].ableToReplicate[1] = 1
+        if not y+1 in energies[x-1]:
+            self.positions[x][y].ableToReplicate[2] = 1
+        if not y-1 in energies[x-1]:
+            self.positions[x][y].ableToReplicate[3] = 1
 
 
 class Cell:
@@ -126,7 +159,7 @@ class Cell:
         self.id = str(id)
         self.energy = 0
         self.dead = False
-        self.ableToReplicate = 1
+        self.ableToReplicate = [0,0,0,0]
 
     def tick(self, food_energy):
         self.energy = food_energy
@@ -144,31 +177,56 @@ class Cell:
     def replicate(self):
         xdir = 1
         ydir = 1
+        cells = []
+        if not self.ableToReplicate == [0,0,0,0]:
+            for i in range(len(self.ableToReplicate)):
+                if self.ableToReplicate[i] == 1:
+                    if i == 0:
+                        xdir = 1
+                        ydir = 1
+                        cells.append(Cell(self.x+xdir, self.y+ydir, uuid.uuid1()))
+                    elif i == 1:
+                        xdir = 1
+                        ydir = -1
+                        cells.append(Cell(self.x+xdir, self.y+ydir, uuid.uuid1()))
+                    elif i == 2:
+                        xdir = -1
+                        ydir = 1
+                        cells.append(Cell(self.x+xdir, self.y+ydir, uuid.uuid1()))
+                    elif i == 3:
+                        xdir = -1
+                        ydir = -1
+                        cells.append(Cell(self.x+xdir, self.y+ydir, uuid.uuid1()))
+        return cells
 
-        if np.random.uniform(-1, 1) < 0:
-            xdir *= -1
-        else:
-            xdir *= 1
-        # if np.random.uniform(-1, 1) < 0:
-        #     ydir *= -1
-        # else:
-        #     ydir *= 1
-            
-        if self.ableToReplicate > 0:
-            return Cell(self.x+xdir, self.y+ydir, uuid.uuid1())
-        return None
+def generate_energy(org, emod, pos):
+    food_energy = collections.defaultdict(dict)
+    for idx in list(org.positions):
+            for idy in list(org.positions[idx]):
+                cell = org.positions[idx][idy]
+                if ticks < 70:
+                    food_energy[cell.x][cell.y] = 1
+                else:
+                    food_energy[cell.x][cell.y] = 0
+                    if cell.y == 100 and cell.x == 380 and pos == None:
+                        food_energy[cell.x][cell.y] = 100*emod
+                    elif pos != None:
+                        food_energy[pos[0]][pos[1]] = 100*emod
+                    if cell.y == 100 and cell.x == 420:
+                        food_energy[cell.x][cell.y] = 100*emod
+    return food_energy
 
-organisms = []
-for i in range(10):
-    organisms.append(Organism([i*80,0]))
 ticks = 0
-energy_by_tick = 1
+org = Organism(ORIGIN)
+lastMousePos = [380, 100]
+emod = 1
 while True:
-    for i in range(10):
-        organisms[i].tick(ticks)
-        render_cells(organisms[i])
-    energy_by_tick += handle_pygame()
+    energy = generate_energy(org, emod, lastMousePos)
+    org.tick(ticks, energy)
+    render_cells(org)
+    (edelta, pos) = handle_pygame()
+    if pos != None: lastMousePos = pos
+    emod += edelta
     pygame.display.update()
 
     ticks += 1
-    energy_by_tick = 0.1
